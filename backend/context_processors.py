a5dc5d7a585994ee28d098ae7ccf1d64ef646bdf
Stefan <hisoka999@gmail.com>
2013-06-19 17:58:55 +0200

    - smilies list in the post creation - display changes for the absolution theme

--- backend/context_processors.py
+++ backend/context_processors.py
@@ -7,9 +7,15 @@ from silverbb import version as ver
 from django.conf import settings
 from functions import get_path
 from django.contrib.sites.models import get_current_site
+from models import Smilie
 
 def theme_path(request):
-    return {'STATIC_THEME':settings.STATIC_URL+get_path(request.user),'BASE_PATH':get_path(request.user)+'base.html','CURRENT_SITE':get_current_site(request),'THEME_PATH':get_path(request.user)}
+    smilies = Smilie.objects.all()
+    return {'STATIC_THEME':settings.STATIC_URL+get_path(request.user)
+            ,'BASE_PATH':get_path(request.user)+'base.html'
+            ,'CURRENT_SITE':get_current_site(request)
+            ,'THEME_PATH':get_path(request.user)
+            ,'smilies':smilies}
 
 def version(request):
     return {'version':ver.sbb_version,'revision':ver.revision()}
